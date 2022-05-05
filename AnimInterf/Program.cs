19414016eb5e4692d64e82f18c0f134f8deb5ffb
Vasco Anjos <91753808+ALL-ISPOT@users.noreply.github.com>
2022-05-05 14:42:19 +0100

    Update

---
+++ AnimInterf/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace AnimInterf
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello world!");
+            Animal();
+        }
+
+        static void Animal()
+        {
+            Console.WriteLine("Que som fazem os morcegos?");
+        }
+    }
+
+    public class Bat
+    {
+        public void Animal()
+        {
+            Console.WriteLine("Que som fazem os morcegos?");
+        }
+    }
+}
